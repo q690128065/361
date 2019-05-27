87e2ff8aef39b13a295751639dfd42c84ad5b136
mahua <wb-lbj544074@alibaba-inc.com>
2019-05-27 15:42:38 +0800

    1
    1

---
+++ build/precache-manifest.b61e59b13219d3882f768dc7ebbdf772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "df882e59036b5cc519ef",
+    "url": "/static/js/main.df882e59.chunk.js"
+  },
+  {
+    "revision": "783652a4b99275f227a6",
+    "url": "/static/js/2.783652a4.chunk.js"
+  },
+  {
+    "revision": "df882e59036b5cc519ef",
+    "url": "/static/css/main.4867ce40.chunk.css"
+  },
+  {
+    "revision": "783652a4b99275f227a6",
+    "url": "/static/css/2.aa693238.chunk.css"
+  },
+  {
+    "revision": "61a48b7697a8c8e5794f23a352ea4f50",
+    "url": "/index.html"
+  }
+];
